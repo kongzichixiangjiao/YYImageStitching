d5e78c60670e52def4a2fdb75d66de718f45f02a
kongzichixiangjiao <58895347@qq.com>
2018-04-08 10:46:19 +0800

    1

--- YYImageStitching/Stitching/Model/YYImageModel.swift
+++ YYImageStitching/Stitching/Model/YYImageModel.swift
@@ -14,9 +14,4 @@ class YYImageModel {
     var isSelected: Bool = false
     var indexPath: IndexPath!
     var image: UIImage?
-<<<<<<< HEAD
-    
-    
-=======
->>>>>>> parent of 6b3c1ec... 修改model
 }
